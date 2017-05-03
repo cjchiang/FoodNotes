584965c42701bbe21e74bb800576afe67c0525d5
Quincy Lam <qlam3@my.bcit.ca>
2017-05-03 04:38:59 -0700

    New Stuff

--- include/header.php
+++ include/header.php
@@ -2,28 +2,37 @@
 <html>
 <head>
 	<title>FoodNotes</title>
-    <link rel="stylesheet" href="..styles/boostrap/min.css"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" href="../styles/bootstrap.min.css"/>
     <link rel="stylesheet" href="../styles/styles.css" />
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <script src="js/jquery-2.1.3.min.js" type="text/javascript"></script>
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
 </head>
     <body>
-        <a href="../index.php"><img id="logo" src="images/apple-158419_960_720.png" alt="Apple"/></a>
-        
-        <!--can use a php statement to see if user is already logged in or not and display stuff-->
-        <div id="mainHeader">
-            <p class="headerRight"><button class="warning">Login</button></p>
-            <p class="headerRight"><button class="warning">Sign up</button></p>
+        <div class="container-fluid">
+            <div class="row topMenuBar">
+                <div class="col-xs-4">
+                    <a href="../index.php"><img id="logo" src="images/apple-158419_960_720.png" alt="Apple"/></a> 
+                </div>
+                <div class="col-xs-8 logAndSign">
+                    <!--can use a php statement to see if user is already logged in or not and display stuff-->
+                    <a href="../login.php"><button class="btn btn-warning" id="login">Login</button></a>
+                    <a href="../signup.php"><button class="btn btn-success" id="signUp">Sign Up</button></a>
+                </div>
+            </div>
+            <div class="row menuBar">
+                <div class="col-xs-4">
+                    <a data-toggle="dropdown" class="dropdown-toggle" href="#"><button class="btn btn-warning" id="Menu">Menu</button></a>
+                        <ul class="dropdown-menu">
+                            <li>Summary</li>
+                            <li>Add Food</li>
+                        </ul>
+                </div>
+                <div class="col-xs-4">
+                    <p>Nothing Here Yet1</p>
+                </div>
+                <div class="col-xs-4">
+                    <p>Nothing Here Yet2</p>
+                </div>
+            </div>
         </div>
-    <hr/>
-        <!-- menu should be around here -->
-        <!--<form method="post">
-            <input list="searchForFood"/>
-                <datalist id="searchForFood">
-                    <option value="Beef"></option>
-                    <option value="Apple"></option>
-                    <option value="Banana"></option>
-                </datalist>
-        </form>-->
-        
-        
-    <hr/>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include("include/header.php"); ?>
     <!-- main body will go here, body tags are already distributed to header and footer-->
-    
+<p>I am home</p>
 
 <?php include("include/footer.php"); ?>
