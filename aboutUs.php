ce50aa8bbe8d8c86c6007c55a2ba05aa9b155fc6
Ron Tran <cbod.devil@gmail.com>
2017-05-07 16:12:23 -0700

    Added About Us page

---
+++ aboutUs.php
@@ -0,0 +1,13 @@
+<?php include("include/header.php"); ?>
+
+
+<div align="center" style="font-size: 16px; width: 60%; margin: auto; text-align:justify">
+    <header>
+    <h1 style="font-family: cursive">If you always worry about overspending and causing too much food wastes? We are here for you!</h1>
+    <img src="images/foodwastesources.PNG"/ style="width: 100%">
+    <h2 style="font-family: impact">We strongly believe that over 50% of wasted food can be avoidable, and we are excited to help you improve your progress of reducing food waste and saving money.</h2>
+    <p style="font-family: Arial">FoodNotes allows the users to realize how much of their food is not eaten, in term of money. We calculate total price of all items according to the statistics from the Government Of Canada. All leftover or unused food is counted towards a wasted total amount. Our enthusiatic and passionate team will send you notifications to remind you about how much your food is not eaten, and encourage you to reduce food waste and save money.</p>    
+    </header>
+</div>
+
+<?php include("include/footer.php"); ?>
