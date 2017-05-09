3904cb7ee5d71f188622a6450e50c7ee16d1ca83
Ron Tran <cbod.devil@gmail.com>
2017-05-09 15:24:12 -0700

    finish related apps page

---
+++ RelatedApps.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<div style="text-align:center">
+<head>
+    <a href="../index.php"><img src="images/logo.png"></img></a>
+    <h1>Related Apps</h1>
+</head>
+</div>
+<?php include("include/header.php"); ?>
+</br>
+<body>
+    <table align="center">
+        <tr>
+            <th>Name Of Apps</th>
+            <th>Description</th>
+        </tr>
+         <tr>
+            <td>1</td>
+            <td>blah blah blah blah</td>
+        </tr>
+         <tr>
+            <td>2</td>
+            <td>blah blah blah blah</td>
+        </tr>
+         <tr>
+            <td>3</td>
+            <td>blah blah blah blah</td>
+        </tr>
+         <tr>
+            <td>4</td>
+            <td>blah blah blah blah</td>
+        </tr>
+         <tr>
+            <td>5</td>
+            <td>blah blah blah blah</td>
+        </tr>
+    </table>
+</body>
+<?php include("include/footer.php"); ?>
+</html>
