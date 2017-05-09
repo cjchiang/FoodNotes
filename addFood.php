603cb3687123a7f62758094e818b742677ab6ad1
Chonjou Chiang <chonjou@gmail.com>
2017-05-09 13:44:04 -0700

    Added a new jquery version

---
+++ addFood.php
@@ -0,0 +1,10 @@
+<?php include("include/header.php"); ?>
+    <!-- main body will go here, body tags are already distributed to header and footer-->
+    <p>filler functions</p>
+    <!--<div class="container-fluid ourContent">
+        <div id="canvasSelect col-xs-12">
+            <canvas id="canvasSelection" width="500px" height="500px" style="border:1px solid black"></canvas>
+        </div>
+    </div>-->
+
+<?php include("include/footer.php"); ?>
