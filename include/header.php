2477b027d1c232d17013e0f3b2e7733a7e210b61
Chonjou Chiang <chonjou@gmail.com>
2017-05-09 15:20:53 -0700

    trying to revert mess

--- include/header.php
+++ include/header.php
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html>
-<head>
-	<title>FoodNotes</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../styles/bootstrap.min.css"/>
-    <link rel="stylesheet" href="../styles/styles.css" />
-    <script src="js/jquery-2.1.3.min.js" type="text/javascript"></script>
-    <script src="js/bootstrap.min.js" type="text/javascript"></script>
-</head>
-    <body>
-        <!--<div class="container-fluid topHeader">
-            <div class="row topMenuBar">
-                <div class="col-xs-4">
-                    <a href="../index.php"><img id="logo" src="images/logo.png" alt="Apple"/></a> 
-                </div>
-                <div class="col-xs-8 logAndSign">
-                    can use a php statement to see if user is already logged in or not and display stuff
-                    <a href="../login.php"><button class="btn btn-warning" id="login">Login</button></a>
-                    <a href="../signup.php"><button class="btn btn-success" id="signUp">Sign Up</button></a>
-                </div>
-            </div>
-            <div class="row menuBar">
-                <div class="col-xs-4">
-                    <p>Nothing Here Yet1</p>
-                </div> 
-				<div class="col-xs-4">
-                    <a data-toggle="dropdown" class="dropdown-toggle" href="#"><button class="btn btn-warning" id="Menu">Menu</button></a>
-                        <ul class="dropdown-menu">
-                            <a href="../summary.php"><li>Summary</li></a>
-                            <a href="../addFood.php"><li>Add Food</li></a>
-                        </ul>
-                </div>
-                <div class="col-xs-4">
-                    <p>Nothing Here Yet2</p>
-                </div>
-            </div>
-        </div>-->
-=======
 <!DOCTYPE html>
 <html>
 <head>
@@ -55,7 +14,7 @@
             <div class="row menuBar">
                 <div class="col-xs-4">
                     <p><a href="../index.php"><button class="btn btn-warning topMenu" id="goBackHome">Home</button></a></p>
-                </div> 
+                </div>
 				<div class="col-xs-4">
                     <a data-toggle="dropdown" class="dropdown-toggle" href="#"><button class="btn btn-warning topMenu">Menu</button></a>
                         <ul class="dropdown-menu">
@@ -70,4 +29,3 @@
                 </div>
             </div>
         </div>
->>>>>>> ba3f702684e424bd9c82d058d744cd3a972459f9
