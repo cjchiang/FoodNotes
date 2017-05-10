f640eef1487924b4b888d57061bd33803f87d2ca
Quincy Lam <qlam3@my.bcit.ca>
2017-05-10 15:34:16 -0700

    pulling from develop again

--- login.php
+++
@@ -1,5 +0,0 @@
-<?php include("include/header.php"); ?>
-    <!-- main body will go here, body tags are already distributed to header and footer-->
-<p>you are logging in</p>    
-<!-- Scripts are useless w.out a  -->
-<?php include("include/footer.php"); ?>
--- records.php
+++
@@ -1,7 +0,0 @@
-<?php include("include/header.php"); ?>
-    <!-- main body will go here, body tags are already distributed to header and footer-->
-<div>
-    <p>records</p>
-</div>
-
-<?php include("include/footer.php"); ?>
