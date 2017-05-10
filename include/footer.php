c015e0b7d01b0d3e7842fe2faad3160838a1df0c
Quincy Lam <qlam3@my.bcit.ca>
2017-05-09 17:07:19 -0700

    button changes v1

--- include/footer.php
+++ include/footer.php
@@ -5,16 +5,16 @@
     </div>
     <div class="row bottomNavi">
         <div class="col-xs-3 visible-xs buttonHolder">
+            <a href="../index.php"><button class="bottomButton"><img class="navImg" src="images/home.png" alt="home"/></button></a>
+        </div>
+        <div class="col-xs-3 visible-xs buttonHolder">
             <a href="../summary.php"><button class="bottomButton"><img class="navImg" src="images/list.png" alt="summary"/></button></a>
         </div>
         <div class="col-xs-3 visible-xs buttonHolder">
             <a href="../addFood.php"><button class="bottomButton"><img class="navImg" src="images/add.png" alt="add"/></button></a>
         </div>
         <div class="col-xs-3 visible-xs buttonHolder">
-            <a href="../settings.php"><button class="bottomButton"><img class="navImg" src="images/settings.png" alt="settings"/></button></a>
-        </div>
-        <div class="col-xs-3 visible-xs buttonHolder">
-            <button class="bottomButton"><img class="navImg" src="images/help.png" alt="help"/></button>
+            <button class="bottomButton"><img class="navImg" src="images/more.png" alt="more"/></button>
         </div>
     </div>
 </div>
