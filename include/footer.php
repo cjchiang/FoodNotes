c60a3912a22751f1f6eda7957ef6a721e40472a8
Quincy Lam <qlam3@my.bcit.ca>
2017-05-10 13:56:27 -0700

    much better bottom bar

--- include/footer.php
+++ include/footer.php
@@ -2,26 +2,26 @@
     <hr/>
     <div class="row">
         <p>&copy;Group 13</p>
-    </div>  
-    <div class="row bottomNavi visible-xs">
-        <div class="col-xs-2 visible-xs buttonHolder">
-            <p id="moveMe"><a href="../index.php"><button class="bottomButton"><img class="navImg" src="images/home.png" alt="home"/></button></a></p>
+    </div> 
+</div>
+<div class="container-fluid">
+    <div class="row visible-xs bottomNavi">
+        <div class="col-xs-2 visible-xs buttonHolder"><a href="../index.php"><button class="bottomButton"><img class="navImg" src="images/home.png" alt="home"/></button></a>
         </div>
         <div class="col-xs-2 visible-xs buttonHolder">
             <a href="../summary.php"><button class="bottomButton"><img class="navImg" src="images/list.png" alt="list"/></button></a>
-        </div>
-         <div class="col-xs-4 visible-xs" id="mainButtonPlus">
-            <a href="../addFood.php"><button class="specialButton"><img class="navImg" src="images/add.png" alt="add"/></button></a>
-        </div>       
-        <div class="col-xs-2 visible-xs buttonHolder">
+        </div>      
+        <div class="col-xs-offset-4 col-xs-2 visible-xs buttonHolder">
             <a href="../records.php"><button class="bottomButton"><img class="navImg" src="images/records.png" alt="records"/></button></a>
         </div>
         <div class="col-xs-2 visible-xs buttonHolder">
             <a href="../more.php"><button class="bottomButton"><img class="navImg" src="images/more.png" alt="more"/></button></a>
         </div>
     </div>
+    <div class="visible-xs" id="specialNavi">
+        <a href="../addFood.php"><button class="specialButton"><img class="navImg" src="images/add.png" alt="add"/></button></a>
+    </div>
 </div>
-<script src="/script/footerFix.js" type="text/javascript">
 </script>
     </body>
 </html>
