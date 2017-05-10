310ae33f0ecae6f142f7abe2cfe284eaf8f4a69c
Quincy Lam <qlam3@my.bcit.ca>
2017-05-09 17:21:55 -0700

    Fixing

---
+++ signup.php
@@ -0,0 +1,5 @@
+<?php include("include/header.php"); ?>
+    <!-- main body will go here, body tags are already distributed to header and footer-->
+<p>you are signing up</p>
+
+<?php include("include/footer.php"); ?>
