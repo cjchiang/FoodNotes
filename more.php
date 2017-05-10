924b2631a83493c4676a26b914c46b8ce3166375
Quincy Lam <qlam3@my.bcit.ca>
2017-05-09 17:33:13 -0700

    extra stuff

---
+++ more.php
@@ -0,0 +1,6 @@
+<?php include("include/header.php"); ?>
+    <!-- main body will go here, body tags are already distributed to header and footer-->
+<div style="background-color: lightgreen;">
+</div>
+
+<?php include("include/footer.php"); ?>
