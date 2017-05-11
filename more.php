d589341753df696855eda36d07d23b3b5ab47c47
Chonjou Chiang <chonjou@gmail.com>
2017-05-10 17:16:42 -0700

    trying to fix branch

--- more.php
+++
@@ -1,6 +0,0 @@
-<?php include("include/header.php"); ?>
-    <!-- main body will go here, body tags are already distributed to header and footer-->
-<div style="background-color: lightgreen;">
-</div>
-
-<?php include("include/footer.php"); ?>
