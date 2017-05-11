f4d40e1b99202d1a39ab843b9f14c32e2ea81d74
Quincy Lam <qlam3@my.bcit.ca>
2017-05-11 16:09:30 -0700

    amazing pages

--- Contact.php
+++ Contact.php
@@ -22,23 +22,41 @@
       </ul>
     </div>
   </nav>
-  <h1 style="text-align: center"><strong>CONTACT US</strong></h1>
+
   <div class="container green lighten-1 ">
-    <form>
-      <label for="firstname">First Name</label>
-      <input type="text" id="fname" name="first_name" placeholder="First Name..">
-      <label for="lastname">Last Name</label>
-      <input type="text" id="lname" name="last_name" placeholder="Last Name..">
-      <label for="email">Email</label>
-      <input type="text" id="email" name="email" placeholder="email">
-      <label for="subject">Subject</label>
-      <textarea id="subject" name="subject" placeholder="Comment/Question" style="height:200px"></textarea>
-      <div class="center-align">
-      <a class="waves-effect waves-light btn green lighten-3">
-        <input type="submit" id="submit1" value="Submit">
-      </a>
+    <div class="row white-text center-align">
+      <h2><strong>Contact Us</strong></h2>
     </div>
-    </form>
+    <div class="row">
+      <form class ="col s12">
+        <div class="row">
+          <div class="input-field col s6">
+            <input type="text" id="first_name">
+            <label for="first_name">First Name</label>
+          </div>
+          <div class="input-field col s6">
+            <input type="text" id="last_name">
+            <label for="last_name">Last Name</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <input id="email" type="email" class="validate">
+            <label for="email">Email</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <textarea id="textarea1" class="materialize-textarea"></textarea>
+            <label for="textarea1">Subject</label>
+          </div>
+        </div>
+        <div class="left-align">
+          <a class="waves-effect waves-light btn green lighten-3">
+            <input type="submit" id="submit1" value="Submit">
+          </a>
+        </div>
+      </form>
+    </div>
   </div>
-  </div>
-  <?php include("include/footer.php"); ?>
+<?php include("include/footer.php"); ?>
