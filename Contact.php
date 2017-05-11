fb13d9f88b4916fadf1ba54fe7d7e636286cec52
Ron Tran <cbod.devil@gmail.com>
2017-05-11 14:40:04 -0700

    hg

--- Contact.php
+++ Contact.php
@@ -23,22 +23,22 @@
     </div>
   </nav>
   <h1 style="text-align: center"><strong>CONTACT US</strong></h1>
-  <div class="container green lighten-1 ">
+  <div class="container green lighten-1">
     <form>
-      <label for="firstname">First Name</label>
-      <input type="text" id="fname" name="first_name" placeholder="First Name..">
-      <label for="lastname">Last Name</label>
+      <label for="firstname" class="active">First Name</label>
+      <input type="text" id="fname"  name="first_name" placeholder="First Name..">
+      <label for="lastname" style="color :white">Last Name</label>
       <input type="text" id="lname" name="last_name" placeholder="Last Name..">
-      <label for="email">Email</label>
+      <label for="email" style="color :white">Email</label>
       <input type="text" id="email" name="email" placeholder="email">
-      <label for="subject">Subject</label>
+      <label for="subject" style="color :white">Subject</label>
       <textarea id="subject" name="subject" placeholder="Comment/Question" style="height:200px"></textarea>
       <div class="center-align">
-      <a class="waves-effect waves-light btn green lighten-3">
-        <input type="submit" id="submit1" value="Submit">
+      <a style=" background-color: #4CAF50;">
+        <input type="submit" id="submit1" value="Submit" style=" background-color: #4CAF50;">
       </a>
     </div>
     </form>
   </div>
-  </div>
+
   <?php include("include/footer.php"); ?>
