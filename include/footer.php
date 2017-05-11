d167ae4dc9fa064b2d56c75ea7a8235081284292
Chonjou Chiang <chonjou@gmail.com>
2017-05-10 17:23:36 -0700

    fixed with materialized

--- include/footer.php
+++ include/footer.php
@@ -2,26 +2,25 @@
     <hr/>
     <div class="row">
         <p>&copy;Group 13</p>
-    </div> 
+    </div>
 </div>
-<div class="container-fluid">
-    <div class="row visible-xs bottomNavi">
-        <div class="col-xs-2 visible-xs buttonHolder"><a href="../index.php"><button class="bottomButton"><img class="navImg" src="images/home.png" alt="home"/></button></a>
+    <div class="row bottomNavi">
+        <div style="padding:0px" class="col s2 buttonHolder"><a href="../index.php"><button class="bottomButton"><img class="navImg" src="images/home.png" alt="home"/></button></a>
         </div>
-        <div class="col-xs-2 visible-xs buttonHolder">
+        <div style="padding:0px" class="col s2 buttonHolder">
             <a href="../summary.php"><button class="bottomButton"><img class="navImg" src="images/list.png" alt="list"/></button></a>
-        </div>      
-        <div class="col-xs-offset-4 col-xs-2 visible-xs buttonHolder">
+        </div>
+        <div style="padding:0px" class="col s2 offset-s4 buttonHolder">
             <a href="../records.php"><button class="bottomButton"><img class="navImg" src="images/records.png" alt="records"/></button></a>
         </div>
-        <div class="col-xs-2 visible-xs buttonHolder">
+        <div style="padding:0px" class="col s2 buttonHolder">
             <a href="../more.php"><button class="bottomButton"><img class="navImg" src="images/more.png" alt="more"/></button></a>
         </div>
     </div>
     <div class="visible-xs" id="specialNavi">
         <a href="../addFood.php"><button class="specialButton"><img class="navImg" src="images/add.png" alt="add"/></button></a>
     </div>
-</div>
+
 </script>
     </body>
 </html>
