afbf340b9827cddd2ce3429eced8c8b973fa55a0
Quincy Lam <qlam3@my.bcit.ca>
2017-05-11 14:46:35 -0700

    stuff

--- aboutUsDevs.php
+++ aboutUsDevs.php
@@ -1,6 +1,21 @@
 <?php include("include/header.php"); ?>
 <div class="container-fluid">
     <hr id="aboutUsHeader"/>
-    <div class=""
+    <div class="row">
+        <div class="col s12 green"><h1 style="font-size: 10vw;">About the Developers</h1></div>
+    </div>
+    <div class="row">
+        <p>
+            This app was made by BCIT CST students for COMP 2910. The purpose for this app is to help address food waste problems by helping users identifying the amount of potential savings that could have been saved if the food was not consumed.
+        </p>
+    </div>
+    <div class ="row">
+        <div class="col m2">
+        Nate<p>Nate is cool</p></div>
+        <div class="col m2">Alex <p>Alex is cool</p></div>
+        <div class="col m2">Ron <p>Ron is cool</p></div>
+        <div class="col m2">LanLan <p>LanLan is cool</p></div>
+        <div class="col m2">Quincy<p>Quincy is cool</p></div>
+    </div>
 </div>
 <?php include("include/footer.php"); ?>
---
+++ affiliatedApps.php
@@ -0,0 +1,6 @@
+<?php include("include/header.php"); ?>
+<div class="row green">
+    <h1>To learn more about food waste, check out these apps that other students made!</h1>
+</div>
+<div class=""
+<?php include("include/footer.php"); ?>
