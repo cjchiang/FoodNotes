d673abc3369c23bcd1b3a0c0968afd29aba44839
Chonjou Chiang <chonjou@gmail.com>
2017-05-10 17:30:11 -0700

    hello 2

---
+++ records.php
@@ -0,0 +1,7 @@
+<?php include("include/header.php"); ?>
+    <!-- main body will go here, body tags are already distributed to header and footer-->
+<div>
+    <p>records</p>
+</div>
+
+<?php include("include/footer.php"); ?>
