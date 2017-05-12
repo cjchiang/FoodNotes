5a511fcf8e10afdbff5dc392d2b4d8e2fc2a80e3
Quincy Lam <qlam3@my.bcit.ca>
2017-05-11 19:23:16 -0700

    fresh new buttons

--- include/footer.php
+++ include/footer.php
@@ -1,25 +1,29 @@
+<br/>
 <div class="container-fluid bottomRow">
     <hr/>
     <div class="row">
         <p>&copy;Group 13</p>
     </div>
 </div>
+<br/>
+<br/>
+<br/>
     <div class="row bottomNavi">
         <div style="padding:0px" class="col s2 buttonHolder">
-          <a href="../index.php"><button class="bottomButton"><img class="navImg" src="images/home.png" alt="home"/></button></a>
+          <a href="../index.php"><button class="btn waves-effect waves-slide green lighten-3 bottomButton" style="height:100%"><img class="navImg" src="images/home.png" alt="home"/></button></a>
         </div>
         <div style="padding:0px" class="col s2 buttonHolder">
-            <a href="../notes.php"><button class="bottomButton"><img class="navImg" src="images/list.png" alt="list"/></button></a>
+            <a href="../notes.php"><button class="btn waves-effect waves-slide green lighten-3 bottomButton" style="height:100%"><img class="navImg" src="images/list.png" alt="list"/></button></a>
         </div>
         <div style="padding:0px" class="col s2 offset-s4 buttonHolder">
-            <a href="../records.php"><button class="bottomButton"><img class="navImg" src="images/records.png" alt="records"/></button></a>
+            <a href="../records.php"><button class="btn waves-effect waves-slide green lighten-3 bottomButton" style="height:100%"><img class="navImg" src="images/records.png" alt="records" style=""/></button></a>
         </div>
         <div style="padding:0px" class="col s2 buttonHolder">
-            <a href="../more.php"><button class="bottomButton"><img class="navImg" src="images/more.png" alt="more"/></button></a>
+            <a href="../settings.php"><button class="btn waves-effect waves-slide green lighten-3 bottomButton" style="height:100%"><img class="navImg" src="images/settings.png" alt="settings"/></button></a>
         </div>
     </div>
-    <div class="visible-xs" id="specialNavi">
-        <a href="../addFood.php"><button class="specialButton"><img class="navImg" src="images/add.png" alt="add"/></button></a>
+    <div id="specialNavi">
+        <a href="../addFood.php"><button class="btn-float waves-effect waves-slide green lighten-3 specialButton"><img class="navImg" src="images/add.png" alt="add"/></button></a>
     </div>
   </div>
     </body>
