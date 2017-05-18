4280450248c97bc76b536c0f412ff26a5fc061b1
Quincy Lam <qlam3@my.bcit.ca>
2017-05-18 15:23:59 -0700

    y

--- more.php
+++
@@ -1,7 +0,0 @@
-<?php include("include/header.php"); ?>
-    <!-- extra links for the future may go here-->
-<div style="background-color: lightgreen;">
-    
-</div>
-
-<?php include("include/footer.php"); ?>
