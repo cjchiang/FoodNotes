9d082a263529396ddb2e2a38897fad708dfc38d6
Alex Xia <xiaalex1998@gmail.com>
2017-05-26 02:29:07 -0700

    added something special for all the devs involved

--- aboutUsDevs.php
+++ aboutUsDevs.php
@@ -4,30 +4,29 @@
     </div>
     <!--Descriptions about the developers-->
     <div class="row center-align flow-text">
+        <div class="container white black-text ">
         This app was made by BCIT CST students for their COMP 2910 project. The purpose for creating this app is to help address food waste problems by helping users identify the amount of potential savings from tracking all the food bits thrown out.
+        </div>
     </div>
         <div class="row center-align flow-text">
-            <div class="col s6">
-                <p>Nate</p>
+            <div class=" col s12" style="text-align: justify">
+                <h5 class="container red black-text col">Nate</h5>
                 <p>Nate is cool guy who likes to code. He enjoys vacationing in South America, which is where he will be working this summer.</p>
             </div>
-            <div class="col s6">
-                <p>Alex</p>
+            <div class="container col s12" style="text-align: justify">
+                <h5 class="container orange darken-3 black-text col">Alex</h5>                
                 <p>Alex likes to code and drinking Mango Bubble Tea. He enjoys not losing sleep, but can't seem to do much of that recently.</p>
             </div>
-        <div class="row center-align">
-            <div class="col s6">
-                <p>Ron</p>
+            <div class="container col s12" style="text-align: justify">
+                <h5 class="container yellow black-text col">Ron</h5>
                 <p>Ron goes shopping at CostCo. He buys 10 apples, but a bug appends them to firebase wrong and now Ron has 10 apples, each for the price of 10 apples. </p>
             </div>
-            <div class="col s6">
-                <p>Lanlan</p>
+            <div class="container col s12" style="text-align: justify">
+                <h5 class="container light-green accent-4 black-text col">Lanlan</h5>
                 <p>Lanlan likes thinking outside the box. A rigid QA tester to boot, Lanlan is the logic provider for the group, and will stop at nothing until she's understood something.</p>
             </div>
-        </div>
-        <div class="row center-align">
-            <div class="col s12">
-                <p>Quincy</p>
+            <div class="container col s12" style="text-align: justify">
+                <h5 class="container orange accent-2 black-text col">Quincy</h5>
                 <p>Quincy is cool, and he likes PHP very much. Sadly, PHP meshes with firebase about as well as wasting food while saving money. Maybe next time, Quincy. </p>
             </div>
         </div>
